880be0fc20851497e616beb4e0fc18f387e4846f
ntieng <noveltieng@gmail.com>
2022-05-31 01:11:58 +0800

    Add transparent and uups upgrade sample contract and deployment script

---
+++ contracts/SimpleTransparentToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract SimpleTransparentToken is Initializable, ERC20Upgradeable, OwnableUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("SimpleTransparentToken", "STT");
+        __Ownable_init();
+    }
+
+    function mintV1(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
---
+++ contracts/SimpleUUPSTokenV2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract SimpleUUPSTokenV2 is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("SimpleUUPSToken", "SUT");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function mintTokenV1(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function mintTokenV2(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+}
